d798308e166b275e4e277bc9ec83bd3ef3bc83bb
MohnishBhujun <mohnishbhujun@gmail.com>
2024-04-26 18:08:59 -0400

    added files

---
+++ client/src/js/database.js
@@ -0,0 +1,43 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Implemented logic to add provided content to the IndexedDB database
+export const putDb = async (content) => {
+  console.log('Put to the database');
+
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('Data saved to the Database', result);
+};
+
+// Implemented logic to retrieve all content from the IndexedDB database
+export const getDb = async () => {
+  // console.log('GET from the database');
+
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+  ? console.log('Data retrieved from database')
+  : console.log('Data not found in the database.');
+
+  return result?.value;
+};
+
+initdb();
